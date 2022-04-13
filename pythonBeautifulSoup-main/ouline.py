fd0463e68990767016fcbe47cb756ce33c0963fd
[jfranze1] <[jfranze1@uncc.edu]>
2022-04-13 12:56:15 -0400

    Added Imports and add inputs for role and location.

---
+++ pythonBeautifulSoup-main/ouline.py
@@ -0,0 +1,29 @@
+#import beautifulsoup and request here
+from bs4 import BeautifulSoup
+import json
+import requests
+def displayJobDetails():
+    print("Display job details")
+
+#function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
+def getJobList(role,location):
+    url = 'https://www.indeed.com/jobs?q={role}&l={location}'
+    # Complete the missing part of this function here 
+
+#save data in JSON file
+def saveDataInJSON(jobDetails):
+    #Complete the missing part of this function here
+    print("Saving data to JSON")
+
+#main function
+def main():
+    # Write a code here to get job location and role from user e.g. role = input()
+    print("Enter role you want to search")
+    role = input()
+    print("Enter location you want to search")
+    location = input()
+    print("Role: " + role + ", Location: " + location)
+    # Complete the missing part of this function here
+    
+if __name__ == '__main__':
+    main()
